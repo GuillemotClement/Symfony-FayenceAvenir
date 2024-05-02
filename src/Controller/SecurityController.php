190aef62c784274c96c04822b023dea55731025c
Clement Glt <guillemot.clement@protonmail.com>
2024-05-02 18:03:40 +0200

    Ajout de authentification. Bug sur le hash du mdp

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Security\Http\Authenticator\FormLoginAuthenticator;
+
+class SecurityController extends AbstractController
+{
+    public function __construct(private FormLoginAuthenticator $authenticator)
+    {
+    }
+
+
+    #[Route('/register', name: 'register')]
+    public function register(UserAuthenticatorInterface $userAuthicator, Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher)
+    {
+        $user = new User();
+        $userForm = $this->createForm(UserType::class, $user);
+        $userForm->handleRequest($request);
+        if($userForm->isSubmitted() && $userForm->isValid()){
+            $hash = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->getPassword($hash);
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'Inscription réussie');
+            return $userAuthicator->authenticateUser($user, $this->authenticator, $request);
+        }
+        return $this->render('security/register.html.twig', [
+            'form'=>$userForm->createView()
+        ]);
+    }
+
+    #[Route('/login', name: 'login')]
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        if($this->getUser()){
+            return $this->redirectToRoute('home');
+        }
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout()
+    {}
+
+}
