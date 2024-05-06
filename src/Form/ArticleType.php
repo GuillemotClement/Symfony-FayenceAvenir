ad258750390d3473ee8513103677008e2d183fe7
Clement Glt <guillemot.clement@protonmail.com>
2024-05-06 10:54:47 +0200

    Design formulaire nouvel article

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -6,6 +6,10 @@ use App\Entity\Article;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +18,25 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('picture')
-            ->add('category')
-        ;
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu'
+            ])
+            ->add('picture', UrlType::class, [
+                'label' => 'Photo',
+            ])
+            ->add('category', ChoiceType::class, [
+                'choices' => [
+                    'Politique' => 'politic',
+                    'Nature' => 'nature',
+                    'Gastonomie' => "food",
+                    'Sport' => 'sport',
+                    'Emploi' => 'work'
+                ],
+                'label' => 'Catégorie',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,7 +14,7 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', null, ['label' => 'Email *'])
+            ->add('email', EmailType::class, ['label' => 'Email *'])
             ->add('password', PasswordType::class, ['label' => 'Mot de passe *'])
             ->add('firstname', null, ['label' => 'Prénom *'])
             ->add('lastname', null, ['label' => 'Nom *'])
